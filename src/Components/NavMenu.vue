4121b6d5a6470561f4546fd895d24c0941395d73
xylxt <isurge@139.com>
2018-09-29 12:11:27 +0800

    Init project

---
+++ src/Components/NavMenu.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+ 
+    <label v-for="(item, index) in menuList">
+      <el-menu-item v-if="item.sub==null" :key="index" @click="openTab(item)" :index="item.menuId">
+        <i :class="item.icon"></i>
+        <span slot="title">{{item.title}}</span>
+      </el-menu-item>
+ 
+      <el-submenu v-if="item.sub" :key="index" :index="item.menuId">
+        <template slot="title">
+          <i :class="item.icon"></i>
+          <span> {{item.title}}</span>
+        </template>
+        <NavMenu :menuList="item.sub" @init_menu="initMenuEx"></NavMenu>
+      </el-submenu>
+    </label>
+ 
+  </div>
+</template>
+ 
+<script>
+  export default {
+    name: 'NavMenu',
+    props: ['menuList'],
+    data() {
+      return {}
+    },
+    methods: {
+      openTab (item) {
+        this.$tab.open(item)
+        this.$emit('init_menu')
+      },
+      initMenuEx () {
+        this.$emit('init_menu')
+      }
+    }
+  }
+</script>
+ 
+<style scoped>
+ 
+</style>
